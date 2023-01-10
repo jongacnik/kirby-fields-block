6e44b6aa17f6ad28e8c6c8f03321df8910e0a321
Thomas Günther <mail@medienbaecker.com>
2023-01-10 15:27:59 +0100

    Rounded corners, collapse icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import FieldsBlock from "./FieldsBlock.vue"
 
 panel.plugin("jg/fields-block", {
+  components: {
+    'k-fields-block-title': {
+      extends: 'k-block-title',
+      template: `
+        <div class="k-block-title" v-on="$listeners">
+          <k-icon :type="icon" class="k-block-icon" />
+          <span class="k-block-name">
+            {{ name }}
+          </span>
+          <span v-if="label" class="k-block-label">
+            {{ label }}
+          </span>
+          <span class="k-fields-block-toggle" v-on:click="$emit('toggle')">
+            <k-icon type="angle-up"/>
+          </span>
+        </div>
+      `
+    }
+  },
   blocks: {
     fields: FieldsBlock
   }
